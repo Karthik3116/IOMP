31d095ba883d336d300e0609c601212776f0f6ef
karthik <127582465+Karthik3116@users.noreply.github.com>
2025-12-05 22:16:21 +0530

    fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://karthik3116k:SDTo3NAOVYvHakrm@cluster0.edbxe4c.mongodb.net/drone_guard?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('uri');
         console.log("✅ MongoDB Connected");
     } catch (err) {
         console.error("❌ Database Connection Error:", err);
